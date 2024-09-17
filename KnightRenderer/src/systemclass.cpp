ed9ca37df1531bd48eca3c63c8b581b6ee9bafe5
jinkunou <743144825@qq.com>
2024-09-17 19:31:41 +0800

    [feat]: Buffers, Shaders, and HLSL.

--- KnightRenderer/src/systemclass.cpp
+++ KnightRenderer/src/systemclass.cpp
@@ -235,7 +235,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 
 	// Create the window with the screen settings and get the handle to it.
 	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName, 
-						    WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
+						    WS_OVERLAPPEDWINDOW,
 						    posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
 
 	// Bring the window up on the screen and set it as main focus.
@@ -243,8 +243,8 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 	SetForegroundWindow(m_hwnd);
 	SetFocus(m_hwnd);
 
-	// Hide the mouse cursor.
-	ShowCursor(false);
+	// Show the mouse cursor.
+	ShowCursor(true);
 
 	return;
 }
